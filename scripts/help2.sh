94ee4124796d78f841dd2914a0d32634d27657bb
Rick M <spookcity138@gmail.com>
2020-03-05 14:09:39 +0100

    Uppp

---
+++ scripts/help2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cat /home/rick/.tmux.conf.local | awk '/^[a-z]/ && last {print $0,"\t",last} {last=""} /^#/{last=$0}' | column -t -s $'\t' | fzf
---
+++ scripts/pull.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Just a simple amateurish script to update some repos
+
+
+echo cd into pf-kerel
+cd /home/rick/downloads/pf-kernel && git pull
+echo cd into polybar-themes
+cd /home/rick/downloads/polybar-themes && git pull
+echo cd into dome
+cd /home/rick/startpages/dome && git pull
+echo cd into homepage
+cd /home/rick/startpages/homepage && git pull
+echo cd into nightTab
+cd /home/rick/startpages/nightTab && git pull
+echo cd into SPECTRUMStartpageV3
+cd /home/rick/startpages/SPECTRUMStartpageV3 && git pull
+echo cd into Startpage
+cd /home/rick/startpages/Startpage && git pull
+echo cd into startpage1
+cd /home/rick/startpages/startpage1 && git pull
+echo cd into tilde-enhanced
+cd /home/rick/startpages/tilde-enhanced && git pull
+echo cd into CustomcssforFirefox
+cd /home/rick/downloads/firefox_css_stuff/CustomCSSforFx && git pull
+echo cd into userchromeTweaks
+cd /home/rick/downloads/firefox_css_stuff/firefox-userchrome.css-tweaks && git pull
+echo cd into MaterialFox
+cd /home/rick/downloads/firefox_css_stuff/MaterialFox && git pull
+echo cd into Tweaks
+cd /home/rick/downloads/firefox_css_stuff/UserChrome-Tweaks && git pull
+echo cd into Superrs-kitchen
+cd /mnt/shit/superrs-kitchen && git pull
+echo cd into ct-ng
+cd /mnt/shit/toolchain_sources/crosstool-ng && git pull
+echo cd into spicetify-themes
+cd /home/rick/downloads/spicetify-themes && git pull
+echo cd into git-open
+cd /home/rick/.oh-my-zsh/custom/plugins/git-open && git pull
+echo cd into zsh-autosuggestions
+cd /home/rick/.oh-my-zsh/custom/plugins/zsh-autosuggestions && git pull
+echo cd into zsh-completions
+cd /home/rick/.oh-my-zsh/custom/plugins/zsh-completions && git pull
+echo cd into zsh-syntax-highlighting
+cd /home/rick/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting && git pull
+echo cd into powerlevel10k
+cd /home/rick/.oh-my-zsh/custom/themes/powerlevel10k && git pull
+echo cd into powerlevel9k
+cd /home/rick/.oh-my-zsh/custom/themes/powerlevel9k && git pull
+echo cd into spaceship-prompt
+cd /home/rick/.oh-my-zsh/custom/themes/spaceship-prompt && git pull
+echo cd into nertree
+cd /home/rick/.vim/bundle/nerdtree && git pull
+echo cd into nerdtree-git
+cd /home/rick/.vim/bundle/nerdtree-git-plugin && git pull
+echo cd into vim-airline
+cd /home/rick/.vim/bundle/vim-airline && git pull
+echo cd into vim-airline-themes
+cd /home/rick/.vim/bundle/vim-airline-themes && git pull
+echo cd into vim-devicons
+cd /home/rick/.vim/bundle/vim-devicons && git pull
+echo cd into vim-gitgutter
+cd /home/rick/.vim/bundle/vim-gitgutter && git pull
+
